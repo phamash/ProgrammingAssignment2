692ac047ce4257aa02c06ee2bf58ab3b8e93848d
liem pham <liempham@PhamilyMac.townisp.com>
2015-08-23 11:32:05 -0400

    committing the files

---
+++ plot5.R
@@ -0,0 +1,28 @@
+if(!exists("NEI")){
+  NEI <- readRDS("summarySCC_PM25.rds")
+}
+if(!exists("SCC")){
+  SCC <- readRDS("Source_Classification_Code.rds")
+}
+
+# merging the two datasets
+if(!exists("NEISCC")){
+  NEISCC <- merge(NEI, SCC, by="SCC")
+}
+
+library(ggplot2)
+
+# How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
+
+subsetNEI <- NEI[NEI$fips=="24510" & NEI$type=="ON-ROAD",  ]
+
+aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
+
+png("plot5.png", width=840, height=480)
+g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") +
+  xlab("year") +
+  ylab(expression('Total PM'[2.5]*" Emissions")) +
+  ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
+print(g)
+dev.off()
